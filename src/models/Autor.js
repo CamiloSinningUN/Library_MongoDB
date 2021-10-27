c0c9d5c73f818dfb41aec2171ef59e5e30d75b01
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-10-27 18:58:01 -0500

    __v fixed

--- src/models/Autor.js
+++ src/models/Autor.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const AutorSchema = new Schema({
-    nombre: {type: String, required: true},
-    apellido1: {type: String, required: true},
-    apellido2: {type: String, required: true},
-}, { collection: 'autor' });
+    nombre: { type: String, required: true },
+    apellido1: { type: String, required: true },
+    apellido2: { type: String, required: true },
+}, {
+    collection: 'autor',
+    versionKey: false
+});
 
-module.exports = mongoose.model('autor',AutorSchema)
+module.exports = mongoose.model('autor', AutorSchema)
